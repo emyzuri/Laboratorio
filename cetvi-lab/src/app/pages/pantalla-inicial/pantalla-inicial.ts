29158791766eff88d6aa8e4fc887a92f9abc37dc
Borys <oaldair316@gmail.com>
2026-01-28 21:38:01 -0500

    maquetación de pantalla principal

---
+++ cetvi-lab/src/app/pages/pantalla-inicial/pantalla-inicial.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pantalla-inicial',
+  imports: [],
+  templateUrl: './pantalla-inicial.html',
+  styleUrl: './pantalla-inicial.scss',
+})
+export class PantallaInicial {
+
+}
